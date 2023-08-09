6e838bf5712168af7dd2e49b9d1a1fc337d14f38
Eldin Zhou <zhouziyi@u.nus.edu>
2023-08-09 15:17:35 +0800

    Batch update exchange (#154)
    * fix:add batch delete
    
    * fix:add batch delete and unit tests
    
    * fix:testsuite writeMode not found
    
    * fix:add update vertices and edges with unit tests
    
    * fix:remove redundant comments for writeVertices and writeEdges

--- exchange-common/src/test/scala/com/vesoft/exchange/common/writer/ServerBaseWriterSuite.scala
+++ exchange-common/src/test/scala/com/vesoft/exchange/common/writer/ServerBaseWriterSuite.scala
@@ -51,6 +51,44 @@ class ServerBaseWriterSuite extends ServerBaseWriter {
   }
 
   @Test
+   def toUpdateExecuteSentenceSuiteForVertex(): Unit = {
+    val vertices: ListBuffer[Vertex] = new ListBuffer[Vertex]
+    val propNames = List("col_string",
+      "col_fixed_string",
+      "col_bool",
+      "col_int",
+      "col_int64",
+      "col_double",
+      "col_date",
+      "col_geo")
+
+    val props1 =
+      List("\"name\"", "\"name\"", true, 10, 100L, 1.0, "2021-11-12", "LINESTRING(1 2, 3 4)")
+    val props2 = List("\"name2\"",
+      "\"name2\"",
+      false,
+      11,
+      101L,
+      2.0,
+      "2021-11-13",
+      "POLYGON((0 1, 1 2, 2 3, 0 1))")
+
+    vertices.append(Vertex("\"vid1\"", props1))
+    vertices.append(Vertex("\"vid2\"", props2))
+    val nebulaVertices = Vertices(propNames, vertices.toList, None)
+
+    val sentence = toUpdateExecuteSentence("person", nebulaVertices)
+    val expectSentence =
+      "UPDATE VERTEX ON `person` \"vid1\" SET `col_string`=\"name\",`col_fixed_string`=\"name\"," +
+        "`col_bool`=true,`col_int`=10,`col_int64`=100,`col_double`=1.0,`col_date`=2021-11-12," +
+        "`col_geo`=LINESTRING(1 2, 3 4);UPDATE VERTEX ON `person` \"vid2\" SET " +
+        "`col_string`=\"name2\",`col_fixed_string`=\"name2\",`col_bool`=false,`col_int`=11," +
+        "`col_int64`=101,`col_double`=2.0,`col_date`=2021-11-13," +
+        "`col_geo`=POLYGON((0 1, 1 2, 2 3, 0 1))"
+    assert(expectSentence.equals(sentence))
+  }
+
+  @Test
   def toExecuteSentenceSuiteForVertexWithSymbol(): Unit = {
     val vertices: ListBuffer[Vertex] = new ListBuffer[Vertex]
     val tagName                      = "person,test_with^symbol#"
@@ -106,6 +144,34 @@ class ServerBaseWriterSuite extends ServerBaseWriter {
     println(sentence)
     println(expectSentence)
     assert(sentence.equals(expectSentence))
+  }
+
+  @Test
+  def toUpdateExecuteSuiteForEdge(): Unit = {
+    val edges: ListBuffer[Edge] = new ListBuffer[Edge]
+    val propNames = List("col_string",
+                         "col_fixed_string",
+                         "col_bool",
+                         "col_int",
+                         "col_int64",
+                         "col_double",
+                         "col_date",
+                         "col_geo")
+    val props1 = List("\"Tom\"", "\"Tom\"", true, 10, 100L, 1.0, "2021-11-12", "POINT(1 2)")
+    val props2 = List("\"Bob\"", "\"Bob\"", false, 20, 200L, 2.0, "2021-05-01", "POINT(2 3)")
+    edges.append(Edge("\"vid1\"", "\"vid2\"", Some(1L), props1))
+    edges.append(Edge("\"vid2\"", "\"vid1\"", Some(2L), props2))
+
+    val nebulaEdges         = Edges(propNames, edges.toList, None, None)
+    val sentence = toUpdateExecuteSentence("friend", nebulaEdges)
+    val expectSentence =
+      "UPDATE EDGE ON `friend` \"vid1\"->\"vid2\"@1 SET `col_string`=\"Tom\"," +
+        "`col_fixed_string`=\"Tom\",`col_bool`=true,`col_int`=10,`col_int64`=100," +
+        "`col_double`=1.0,`col_date`=2021-11-12,`col_geo`=POINT(1 2);" +
+        "UPDATE EDGE ON `friend` \"vid2\"->\"vid1\"@2 SET `col_string`=\"Bob\"," +
+        "`col_fixed_string`=\"Bob\",`col_bool`=false,`col_int`=20,`col_int64`=200," +
+        "`col_double`=2.0,`col_date`=2021-05-01,`col_geo`=POINT(2 3)"
+    assert(expectSentence.equals(sentence))
   }
 
   @Test
