aa1978c3c4169a4f355f2d2e23cd1a1a5e830264
Anqi <anqi.wang@vesoft.com>
2023-11-06 03:53:47 -0600

    do not repartition when source dataframe has the same partition number with write partition config (#173)

--- nebula-exchange_spark_3.0/src/main/scala/com/vesoft/nebula/exchange/Exchange.scala
+++ nebula-exchange_spark_3.0/src/main/scala/com/vesoft/nebula/exchange/Exchange.scala
@@ -433,7 +433,9 @@ object Exchange {
   private[this] def repartition(frame: DataFrame,
                                 partition: Int,
                                 sourceCategory: SourceCategory.Value): DataFrame = {
-    if (partition > 0 && !CheckPointHandler.checkSupportResume(sourceCategory)) {
+    val currentPart = frame.rdd.partitions.length
+    if (partition > 0 && currentPart != partition
+        && !CheckPointHandler.checkSupportResume(sourceCategory)) {
       frame.repartition(partition).toDF
     } else {
       frame
